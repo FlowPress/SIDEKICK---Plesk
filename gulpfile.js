e00d50b351bf0f546c9bc221057d73ddcea6651e
Bart <bart@flowpress.ca>
2015-11-09 08:36:08 -0500

    removed logs / gulp and updated version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 var watch = require('gulp-watch');
 var zip = require('gulp-zip');
 var fs = require('fs');
@@ -6,45 +6,49 @@ var clean = require('gulp-clean');
 var GulpSSH = require('gulp-ssh');
 
 var config = {
-	host: 'localhost.localdomain',
-	port: 22,
-	username: 'root',
-	privateKey: fs.readFileSync('/Users/bartdabek/.ssh/id_rsa')
+    host: 'localhost.localdomain',
+    port: 22,
+    username: 'root',
+    privateKey: fs.readFileSync('/Users/bartdabek/.ssh/id_rsa')
 }
 
 var gulpSSH = new GulpSSH({
-	ignoreErrors: false,
-	sshConfig: config
-	})
+    ignoreErrors: false,
+    sshConfig: config
+})
 
 
-gulp.task('install_ext',['build'], function () {
-	return gulpSSH
-	.shell(['/usr/local/psa/bin/extension -u sidekick ; /usr/local/psa/bin/extension -i /mnt/hgfs/dist/sidekick_plesk_extension.zip'], {filePath: 'shell.log'})
-	.pipe(gulp.dest('logs'))
-	});
+gulp.task('install_ext', ['build'], function() {
+    return gulpSSH
+        .shell(['/usr/local/psa/bin/extension -u sidekick ; /usr/local/psa/bin/extension -i /mnt/hgfs/dist/sidekick_plesk_extension.zip'], {
+            filePath: 'shell.log'
+        })
+        .pipe(gulp.dest('logs'))
+});
 
 
-gulp.task('clean', function () {
-	return gulp.src('dist/**', {read: false})
-	.pipe(clean());
-	});
+gulp.task('clean', function() {
+    return gulp.src('dist/**', {
+            read: false
+        })
+        .pipe(clean());
+});
 
-gulp.task('build',['clean'], function () {
-	return gulp.src(['**/*',
-		'!node_modules{,/**}'
-		])
-	.pipe(zip('sidekick_plesk_extension.zip'))
-	.pipe(gulp.dest('dist'));
-	});
+gulp.task('build', ['clean'], function() {
+    return gulp.src(['**/*',
+            '!node_modules{,/**}',
+            '!gulpfile.js',
+            '!logs{,/**}',
+            '!logs'
+        ])
+        .pipe(zip('sidekick_plesk_extension.zip'))
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('watch', function() {
-	gulp.watch('**/*.php', ['install_ext'])
-	gulp.watch('**/*.phtml', ['install_ext']);
-	});
+    gulp.watch('**/*.php', ['install_ext'])
+    gulp.watch('**/*.phtml', ['install_ext']);
+});
 
 
-gulp.task('default', ['watch','install_ext']);
-
-
-
+gulp.task('default', ['watch', 'install_ext']);
