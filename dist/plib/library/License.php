be435fde623206716c7ca402cd17a5769d9a75d9
Bart Dabek <bart@flowpress.com>
2017-09-07 14:10:49 -0400

    dist generation

---
+++ dist/plib/library/License.php
@@ -0,0 +1,28 @@
+<?php
+
+class Modules_Sidekick_License
+{
+	public function getPleskLicense()
+	{
+		return $this->getLicense('plesk');
+	}
+
+	public function getWordPressLicense()
+	{
+		return $this->getLicense('wordpress');
+	}
+
+	private function getLicense($type)
+	{
+		$licenses = pm_License::getAdditionalKeysList('ext-sidekick');
+		if (0 == count($licenses)) {
+			return null;
+		}
+		$license = reset($licenses);
+		$keyBody = json_decode($license['key-body'], true);
+		if (!isset($keyBody[$type]) || empty($keyBody[$type])) {
+			return null;
+		}
+		return $keyBody[$type];
+    }
+}
